058811aea31afa36755efe73c3c3e81a44bcdabc
unknown <patticus.tv@gmail.com>
2022-01-28 01:44:45 +1100

    commiting with package.json, inquirer installed, should ignore modules

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,38 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = 
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Please enter the title of your project'
+
+        },
+        {
+            type: 'input',
+            name: 'description-motivation',
+            message: 'What was your motivation for this project?'
+        },
+        {
+            type: 'input',
+            name: 'description-why',
+            message: 'Why did you create this project?'
+        },
+        {
+            type: 'input',
+            name: 'description-problem',
+            message: 'What problem does this project solve?'
+        },
+        {
+            type: 'input',
+            name: 'description-problem',
+            message: 'What problem does this project solve?'
+        },
+
+    ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
