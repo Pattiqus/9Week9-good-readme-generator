2dfc24b42906ff748d572c494d18d5908b5e6f0c
unknown <patticus.tv@gmail.com>
2022-01-29 22:09:31 +1100

    added generate markdown fucntion and wirting functions, have callback error

--- index.js
+++ index.js
@@ -1,78 +1,80 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'Title',
-            message: 'Please enter the title of your project.'
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter the title of your project.'
 
-        },
-        {
-            type: 'input',
-            name: 'Description',
-            message: 'Please enter a detailed description of your project'
-        },
-        {
-            type: 'input',
-            name: 'Table of contents',
-            message: 'Table of Contents'
-        },
-        {
-            type: 'input',
-            name: 'Installation',
-            message: 'What else is the user required to install to run this application?'
-        },
-        {
-            type: 'input',
-            name: 'Usage',
-            message: 'Please leave some short & detailed instructions on how the user can use this application.'
-        },
-        {
-            type: 'input',
-            name: 'Usage',
-            message: 'Please leave some instructions on how the user can use this application'
-        },
-        {
-            type: 'input',
-            name: 'Contributing',
-            message: 'Who contributed to this project?'
-        },
-        {
-            type: 'input',
-            name: 'Tests',
-            message: 'What commands are needed in order to test this application'
-        },
-        {
-            type: 'input',
-            name: 'Tests',
-            message: 'What commands are needed in order to test this application'
-        },
-        {
-            type: 'input',
-            name: 'Licence',
-            message: 'Which Licences are being used for this application?'
-        },
-        {
-            type: 'input',
-            name: 'Questions-github',
-            message: 'What is your GitHub username?'
-        },
-        {
-            type: 'input',
-            name: 'Questions-email',
-            message: 'What is your email?'
-        },
-    ]);
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a detailed description of your project'
+    },
+    // {
+    //     type: 'input',
+    //     name: 'table of contents',
+    //     message: 'Table of Contents'
+    // },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What else is the user required to install to run this application?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please leave some short & detailed instructions on how the user can use this application.'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Who contributed to this project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What commands are needed in order to test this application'
+    },
+    {
+        type: 'checkbox',
+        name: 'licence',
+        message: 'Which Licences are being used for this application?',
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense']
+    },
+    {
+        type: 'input',
+        name: 'questionsGithub',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'questionsEmail',
+        message: 'What is your email?'
+    }
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    // let fileName = `README.md`;
+    fs.writeFile(fileName, data)
+        console.log(fileName);
+        console.log(data);
+        (err) => err ? console.log(err) : console.log('Success!')
+};
 
 // TODO: Create a function to initialize application
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then((data) => {
+            writeToFile("README.md", generateMarkdown(data))
+        });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,29 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+https://github.com/${data.questionsGithub}/${data.title}
+# Description
+${data.description}
+# Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Licence](#licence)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#tests)
+# Installation
+The following dependencies are necessary to run this application: ${data.installation}
+# Usage
+In order to use this application please follow these instructions ${data.usage}
+# Licence
+This project is licenced under ${data.licence}.
+![LICENCE](xxxxxxxxxxxx)
+# Contributing
+Contributors: ${data.contributing}
+# Tests
+The following instructions should be followed for testing of this application: ${data.tests}
+# Questions
+If you have any qestions regrading the repository or the project please contact: GitHub: ${data.questionsGithub} Email: ${data.questionsEmail}
 `;
 }
 
